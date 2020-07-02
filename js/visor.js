98762253d2079a1919f469c5d6730887d2cf078f
Hernan <hernan.lopez@gmail.com>
2020-07-02 10:59:34 -0300

    Primer mapa

---
+++ js/visor.js
@@ -0,0 +1,17 @@
+/*Ubicacion mapa*/
+var mapa = new L.map('mi-mapa', {
+    center: [20, 50],
+    zoom: 2    
+});
+/*Capas de fondo*/
+var capaOSM = new L.tileLayer('http://tile.openstreetmap.org/{z}/{x}/{y}.png');
+var capaRelieve = new L.tileLayer('http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg');
+
+capaOSM.addTo(mapa);
+var capasBase = {
+    "Relieve": capaRelieve,
+    "OpenStreetMap": capaOSM
+};
+
+var selectorCapas = new L.control.layers(capasBase);
+selectorCapas.addTo(mapa);
